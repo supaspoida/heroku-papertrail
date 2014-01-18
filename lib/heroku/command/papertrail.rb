4908be3f4004e1da7a3a841c1f13deeacb5a7dde
Lar Van Der Jagt <dev+lar@supaspoida.com>
2014-01-18 16:08:31 -0500

    No more logging

--- lib/heroku/command/papertrail.rb
+++ lib/heroku/command/papertrail.rb
@@ -6,7 +6,6 @@ class Heroku::Command::Papertrail < Heroku::Command::Run
   #
   # open papertrail web interface
   def open
-    puts Heroku::Command.global_options[:remote].inspect
     system("heroku addons:open papertrail")
   end
   alias_command 'papertrail', 'papertrail:open'
